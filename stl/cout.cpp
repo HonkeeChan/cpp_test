b3f0b4a3edc52e76ef5b9b0c8ba0116f8e40883b
chenhongjian <chj@qq.com>
2017-03-29 20:12:25 +0800

    change cout.cpp

--- stl/cout.cpp
+++ stl/cout.cpp
@@ -6,6 +6,9 @@ int main(){
     float f = 13.1415926;
     cout << "precision(2)" << setprecision(2) << f << endl;
     cout << "precision(3)" << setprecision(3) << f << endl;
-    cout << "precision(5)" << setprecision(5) << f << endl;
+    //fixed 使用小数计数法
+    //scientific 使用科学计数法
+    cout << "precision(5)" << fixed << setprecision(5) << f << endl;
+    cout << "precision(5)" << scientific << setprecision(5) << f << endl;
     return 0;
 }
